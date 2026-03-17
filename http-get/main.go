5ce25b4122303f1372798b8b972230db7de7aa42
Jason Harrington <jason.m.harrington@gmail.com>
2026-03-16 22:27:58 -0400

    add url buildup

--- http-get/main.go
+++ http-get/main.go
@@ -8,14 +8,28 @@ import (
 )
 
 func main() {
-	res, err := http.Get("http://www.google.com/robots.txt")
+	protocol := "http"
+	domain := "www.google.com"
+	path := "/robots.txt"
+
+	url := fmt.Sprintf(
+		"%s://%s%s",
+		protocol,
+		domain,
+		path,
+	)
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	body, err := io.ReadAll(res.Body)
 	res.Body.Close()
 	if res.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		log.Fatalf(
+			"Response failed with status code: %d and\nbody: %s\n",
+			res.StatusCode,
+			body,
+		)
 	}
 	if err != nil {
 		log.Fatal(err)
