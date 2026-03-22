f60e9cfeb0873857877fe7f79d4f1c5f0b74fd69
Jason Harrington <jason.m.harrington@gmail.com>
2026-03-22 02:06:40 -0400

    Merge json-decode into pokeapi for further development in relation to pokedex

--- pokeapi/main.go
+++ pokeapi/main.go
@@ -1,24 +1,45 @@
-package main
-
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	res, err := http.Get("http://www.google.com/robots.txt")
+	type LocationAreaPointer struct {
+		Name string `json:"name"`
+		Url string `json:"url"`
+	}
+	type LocationAreaPointers struct {
+		Count int `json:"count"`
+		Next string `json:"next"`
+		Previous string `json:"previous"`
+		Results []LocationAreaPointer `json:"results"`
+	}
+
+	url := "https://pokeapi.co/api/v2/location-area/"
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	body, err := io.ReadAll(res.Body)
-	res.Body.Close()
-	if res.StatusCode > 299 {
-		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-	}
+
+	var locationAreaPointers LocationAreaPointers
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&locationAreaPointers)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to decode response body: %s", err)
 	}
-	fmt.Printf("%s", body)
+
+	if res.StatusCode > 299 {	
+		log.Fatalf("response failed with status code: %d", res.StatusCode)
+	}
+	
+	fmt.Println("---- Things ----")
+	fmt.Printf("%s -> %d\n", "count", locationAreaPointers.Count)
+	fmt.Printf("%s -> %s\n", "next page", locationAreaPointers.Next)
+	fmt.Printf("%s -> %s\n", "previous page", locationAreaPointers.Previous)
+	for _, result := range locationAreaPointers.Results {
+		fmt.Printf("%s -> %s\n", "name", result.Name)
+		fmt.Printf("%s -> %s\n", "url", result.Url)
+	}
 }
