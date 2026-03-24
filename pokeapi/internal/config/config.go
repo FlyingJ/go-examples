a0fb63b74d57b12d0499f09aaee6f33ae8409b7d
Jason Harrington <jason.m.harrington@gmail.com>
2026-03-23 21:04:22 -0400

    Update config to fit new JSON request schema

--- pokeapi/internal/config/config.go
+++ pokeapi/internal/config/config.go
@@ -1,11 +1,15 @@
 package config
 
 type Config struct{
+	BaseURL string
+	APIPath string
 	Next string
 	Previous string
 }
 
 func (c *Config) Init() {
-	c.Next = "https://pokeapi.co/api/v2/location-area/"
+	c.BaseURL = "https://pokeapi.co"
+	c.APIPath = "/api/v2"
+	c.Next = c.BaseURL + c.APIPath + "/location-area/"
 	c.Previous = ""
 }
--- pokeapi/internal/config/config_test.go
+++ pokeapi/internal/config/config_test.go
@@ -9,10 +9,25 @@ func TestConfig(t *testing.T) {
 	var c config.Config
 	c.Init()
 
-	expectedUrl := "https://pokeapi.co/api/v2/location-area/"
-	if c.Next != expectedUrl {
-		t.Errorf("c.Next not correctly set: %s should be %s", c.Next, expectedUrl)
+	var expectation string
+
+	expectation = "https://pokeapi.co"
+	if c.BaseURL != expectation {
+		t.Errorf("c.BaseURL not correctly set: %s should be %s", c.BaseURL, expectation)
 	}
-	if c.Previous != "" {
+
+	expectation = "/api/v2"
+	if c.APIPath != expectation {
+		t.Errorf("c.APIPath not correctly set: %s should be %s", c.APIPath, expectation)
+	}
+
+	expectation = "https://pokeapi.co/api/v2/location-area/"
+	if c.Next != expectation {
+		t.Errorf("c.Next not correctly set: %s should be %s", c.Next, expectation)
+	}
+
+	expectation = ""
+	if c.Previous != expectation {
+		t.Errorf("c.Previous not correctly set: %s supposed to be %s", c.Previous, expectation)
 	}
 }
