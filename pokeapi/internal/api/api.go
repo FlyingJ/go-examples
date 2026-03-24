cf6a61233cb477a4b8945f7b10b781643b972739
Jason Harrington <jason.m.harrington@gmail.com>
2026-03-23 21:42:38 -0400

    refactor continues

--- pokeapi/internal/api/api.go
+++ pokeapi/internal/api/api.go
@@ -1,1 +1,43 @@
 package api
+
+// the Pokeapi is pleasant enough to provide a few simple response
+// structures
+type APIResource struct{
+	Url      string `json:"url"`
+}
+type NamedAPIResource struct{
+	Name     string `json:"name"`
+	Url      string `json:"url"`
+}
+type APIResourceList[T any] struct{
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []T    `json:"results"`
+}
+
+type Client struct {
+	cfg        Config
+	httpClient *http.Client
+}
+
+type Endpoint struct{
+	Named bool
+	Path  string
+}
+
+var endpoints = map[string]Endpoint{
+	"location": {Path: "/location", Named: true}
+	"location-area":  {Path: "/location-area", Named: true},
+	"pokemon": {Path: "/pokemon", Named: true},
+	"region": {Path: "/region", Named: true},
+}
+
+func GetJSON[T any](ctx context.Context, c *Client, ep Endpoint) (*T, error) {
+	// build URL
+	// make request
+	// check status
+	// decode into T
+	// return &value
+	return nil, fmt.Errorf("not implemented error")
+}
--- pokeapi/main.go
+++ pokeapi/main.go
@@ -2,58 +2,12 @@ package main
 
 import (
 	"pokeapi/internal/api"
+	"pokeapi/internal/config"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
-
-type Config struct {
-	BaseURL string
-	APIPath string
-}
-
-type Client struct {
-	cfg        Config
-	httpClient *http.Client
-}
-
-type Endpoint struct{
-	Named bool
-	Path  string
-}
-
-func GetJSON[T any](ctx context.Context, c *Client, ep Endpoint) (*T, error) {
-	// build URL
-	// make request
-	// check status
-	// decode into T
-	// return &value
-	return nil, fmt.Errorf("not implemented error")
-}
-
-// the Pokeapi is pleasant enough to provide a few simple response
-// structures
-type APIResource struct{
-	Url      string `json:"url"`
-}
-type NamedAPIResource struct{
-	Name     string `json:"name"`
-	Url      string `json:"url"`
-}
-type APIResourceList[T any] struct{
-	Count    int    `json:"count"`
-	Next     string `json:"next"`
-	Previous string `json:"previous"`
-	Results  []T    `json:"results"`
-}
-
-var endpoints = map[string]Endpoint{
-	"location": {Path: "/location", Named: true}
-	"location-area":  {Path: "/location-area", Named: true},
-	"pokemon": {Path: "/pokemon", Named: true},
-	"region": {Path: "/region", Named: true},
-}
 
 func main() {
 	err := api.listLocationAreas()
@@ -70,13 +24,5 @@ func main() {
 
 	if res.StatusCode > 299 {	
 		log.Fatalf("response failed with status code: %d", res.StatusCode)
-	}
-	
-	fmt.Printf("Response from %s\n", url)
-	fmt.Printf("count    -> %d\n", locationAreas.Count)
-	fmt.Printf("next     -> %s\n", locationAreas.Next)
-	fmt.Printf("previous -> %s\n", locationAreas.Previous)
-	for _, locationArea := range locationAreas.Results {
-		fmt.Printf("name: %s -> url: %s\n", locationArea.Name, locationArea.Url)
 	}
 }
